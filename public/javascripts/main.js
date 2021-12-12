e688da2011350b922b2b1e7111150468ce5fd3db
Brandon Young <robotgenis@gmail.com>
2021-12-11 21:43:58 -0500

    progress

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,26 +1,26 @@
 
-var test = `
-             k;double sin()
-         ,cos();main(){float A=
-       0,B=0,i,j,z[1760];char b[
-     1760];printf("\\x1b[2J");for(;;
-  ){memset(b,32,1760);memset(z,0,7040)
-  ;for(j=0;6.28>j;j+=0.07)for(i=0;6.28
- >i;i+=0.02){float c=sin(i),d=cos(j),e=
- sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*
- h*e+f*g+5),l=cos      (i),m=cos(B),n=s\\
-in(B),t=c*h*g-f*        e;int x=40+30*D*
-(l*h*m-t*n),y=            12+15*D*(l*h*n
-+t*m),o=x+80*y,          N=8*((f*e-c*d*g
- )*m-c*d*e-f*g-l        *d*n);if(22>y&&
- y>0&&x>0&&80>x&&D>z[o]){z[o]=D;;;b[o]=
- ".,-~:;=!*#$@"[N>0?N:0];}}/*#****!!-*/
-  printf("\\x1b[H");for(k=0;1761>k;k++)
-   putchar(k%80?b[k]:10);A+=0.04;B+=
-     0.02;}}/*****####*******!!=;:~
-       ~::==!!!**********!!!==::-
-         .,~~;;;========;;;:~-.
-             ..,--------,*/`
+// var test = `
+//              k;double sin()
+//          ,cos();main(){float A=
+//        0,B=0,i,j,z[1760];char b[
+//      1760];printf("\\x1b[2J");for(;;
+//   ){memset(b,32,1760);memset(z,0,7040)
+//   ;for(j=0;6.28>j;j+=0.07)for(i=0;6.28
+//  >i;i+=0.02){float c=sin(i),d=cos(j),e=
+//  sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*
+//  h*e+f*g+5),l=cos      (i),m=cos(B),n=s\\
+// in(B),t=c*h*g-f*        e;int x=40+30*D*
+// (l*h*m-t*n),y=            12+15*D*(l*h*n
+// +t*m),o=x+80*y,          N=8*((f*e-c*d*g
+//  )*m-c*d*e-f*g-l        *d*n);if(22>y&&
+//  y>0&&x>0&&80>x&&D>z[o]){z[o]=D;;;b[o]=
+//  ".,-~:;=!*#$@"[N>0?N:0];}}/*#****!!-*/
+//   printf("\\x1b[H");for(k=0;1761>k;k++)
+//    putchar(k%80?b[k]:10);A+=0.04;B+=
+//      0.02;}}/*****####*******!!=;:~
+//        ~::==!!!**********!!!==::-
+//          .,~~;;;========;;;:~-.
+//              ..,--------,*/`
 
 const SCROLL_RIGHT = 0;
 const SCROLL_DOWN = 1;
@@ -79,7 +79,7 @@ function generateScroll(atEnd = false){
 }
 
 $(document).ready(function(){
-    $(".code").text(test);
+    // $(".code").text(test);
 
     //Create initial page
     newPanel("#item-1");
