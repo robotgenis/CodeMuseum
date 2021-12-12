caf8291b2e1e6f9a5d048e421d6236de6377cb0f
Brandon Young <robotgenis@gmail.com>
2021-12-12 18:08:44 -0500

    jsf

--- routes/library.js
+++ routes/library.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('library/torus');
+  console.log(req.params);
+  res.render('library/jsf');
 });
 
 module.exports = router;
